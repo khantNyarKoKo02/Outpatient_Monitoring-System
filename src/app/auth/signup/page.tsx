3eda3bf7bf6848cb929d17a36852c753b88a690e
khantNyarKoKo02 <6631503061@lamduan.mfu.ac.th>
2024-10-19 15:16:06 +0700

    sign up page

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,46 +1,49 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { useEffect, useState } from "react";
-import loginBackground from "../../../public/images/loginCover.png";
+import loginBackground from "../../../public/images/authCover.png";
 import Image from "next/image";
 import { RiEyeCloseLine, RiEyeLine } from "react-icons/ri";
 import toast from "react-hot-toast";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useSigninUser } from "@/app/hooks/useAuthApi";
+import { useSignupUser } from "@/app/hooks/useAuthApi";
 import { getToken, login } from "@/service/authService";
-import { signInDataType } from "@/types/userTypes";
+import { signUpDataType } from "@/types/userTypes";
 import Link from "next/link";
 
-const Login: React.FC = () => {
+const Signup: React.FC = () => {
   const router = useRouter();
-  const [user, setUser] = useState<signInDataType>({
+  const [user, setUser] = useState<signUpDataType>({
     username: "",
+    email: "",
     password: "",
+    confirmPassword: "",
   });
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [loading, setLoading] = useState(false);
-  const signInMutation = useSigninUser();
+  const signUpMutation = useSignupUser();
 
   useEffect(() => {
     const handleSuccess = async () => {
-      if (signInMutation.isSuccess) {
+      if (signUpMutation.isSuccess) {
         const authToken = getToken();
         if (authToken) login(authToken);
-        toast.success("Login success");
-        router.push("/");
+        toast.success("Signup success");
+        router.push("/auth/login");
       }
     };
-    if (signInMutation.isSuccess) {
+    if (signUpMutation.isSuccess) {
       handleSuccess();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [signInMutation.isSuccess]);
+  }, [signUpMutation.isSuccess]);
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
-    signInMutation.mutate(user, {
+    signUpMutation.mutate(user, {
       onError: (error: any) => {
         toast.error(error.message);
       },
@@ -53,17 +56,10 @@ const Login: React.FC = () => {
   return (
     <div className="flex h-screen">
       {/* Left section with image */}
-      <Image
-        src={loginBackground}
-        alt="loginBackground"
-        className="hidden md:flex md:w-1/2 bg-cover bg-center"
-      />
-
-      {/* Right section with form */}
       <div className="flex w-full md:w-1/2 justify-center items-center h-screen bg-white">
         <form onSubmit={handleLogin} className="w-80 p-6">
           <h3 className="text-3xl text-black font-semibold text-center mb-8 font-noto">
-            Login
+            Sign Up
           </h3>
 
           <div className=" flex flex-col justify-center items-center gap-6">
@@ -72,9 +68,18 @@ const Login: React.FC = () => {
               value={user.username}
               onChange={(e) => setUser({ ...user, username: e.target.value })}
               className="w-[300px] text-black px-4 py-2 border border-primary rounded-lg  focus:outline-none focus:ring-2 focus:ring-primaryactive"
-              placeholder="Username or Email"
+              placeholder="Username"
               required
-              aria-label="Username or Email"
+              aria-label="Username"
+            />
+            <input
+              type="text"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              className="w-[300px] text-black px-4 py-2 border border-primary rounded-lg  focus:outline-none focus:ring-2 focus:ring-primaryactive"
+              placeholder="Email"
+              required
+              aria-label="Email"
             />
 
             <div className="relative w-[300px]">
@@ -96,34 +101,50 @@ const Login: React.FC = () => {
                 {showPassword ? <RiEyeCloseLine /> : <RiEyeLine />}
               </button>
             </div>
-
-            <div className="flex items-center justify-between w-[300px]">
-              <label className="flex items-center">
-                <input type="checkbox" className="mr-2" />
-                <span className="text-gray-600 text-sm">Remember me</span>
-              </label>
-              <a href="#" className="text-primary hover:underline text-sm">
-                Forgot Password?
-              </a>
+            <div className="relative w-[300px]">
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                value={user.confirmPassword}
+                onChange={(e) =>
+                  setUser({ ...user, confirmPassword: e.target.value })
+                }
+                className="w-full text-black px-4 py-2 border border-primary rounded-lg focus:outline-none focus:ring-2 focus:ring-primaryactive"
+                placeholder="Confirm Password"
+                required
+                aria-label="Confirm Password"
+              />
+              <button
+                type="button"
+                onClick={() => setShowConfirmPassword((prev) => !prev)}
+                className="absolute right-3 top-3.5 text-primary focus:outline-none"
+                aria-label={
+                  showConfirmPassword ? "Hide password" : "Show password"
+                }
+              >
+                {showConfirmPassword ? <RiEyeCloseLine /> : <RiEyeLine />}
+              </button>
             </div>
-            <div>
             <div className="flex items-center justify-center gap-2 mb-4 w-[300px]">
-              <p className=" text-slate-500 text-sm">{"Don't have an account?"}</p>
-              <Link href="/auth/signup" className="text-sm text-primary underline">Signup</Link>
+              <p className=" text-slate-500 text-sm"> Already have an account?</p>
+              <Link href="/auth/login" className="text-sm text-primary underline">Login</Link>
             </div>
             <button
               type="submit"
               className="w-full bg-primary text-white py-2 rounded-lg hover:bg-primaryhover active:bg-primartactive transition"
             >
-              {loading ? <Loader2 className="animate-spin" /> : "Log in"}
+              {loading ? <Loader2 className="animate-spin" /> : "Sign Up"}
             </button>
-            </div>
-           
           </div>
         </form>
       </div>
+      {/* Right section with form */}
+      <Image
+        src={loginBackground}
+        alt="loginBackground"
+        className="hidden md:flex md:w-1/2 bg-cover bg-center"
+      />
     </div>
   );
 };
 
-export default Login;
+export default Signup;
--- src/app/hooks/useAuthApi.ts
+++ src/app/hooks/useAuthApi.ts
@@ -1,9 +1,15 @@
-import { signInDataType } from "@/types/userTypes"
+import { signInDataType, signUpDataType } from "@/types/userTypes"
 import { useMutation } from "react-query"
-import { signInUser } from "../api/authApi"
+import { signInUser, signUpUser } from "../api/authApi"
 
 export const useSigninUser = () => {
     return useMutation({
         mutationFn: (user : signInDataType) => signInUser({user})
+    })
+}
+
+export const useSignupUser = () => {
+    return useMutation({
+        mutationFn: (user : signUpDataType) => signUpUser({user})
     })
 }
